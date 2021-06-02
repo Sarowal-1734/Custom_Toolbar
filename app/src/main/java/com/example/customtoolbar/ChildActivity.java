4f0d7da15f05ef7d537e11748b7eb732a5bd9014
sarowalhossain <sarowalhossain1997@gmail.com>
2021-06-02 16:42:59 +0600

    Initial commit...

---
+++ app/src/main/java/com/example/customtoolbar/ChildActivity.java
@@ -0,0 +1,27 @@
+package com.example.customtoolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onButtonClicked(View view) {
+        onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/example/customtoolbar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.customtoolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView textView = findViewById(R.id.appbar_textview_profile);
+
+        // this doesn't work
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void onButtonClicked(View view) {
+        startActivity(new Intent(this, ChildActivity.class));
+    }
+}
