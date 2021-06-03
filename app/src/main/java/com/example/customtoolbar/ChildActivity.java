2ef24f407b2e3d9e863e1e23d86a2396e31bab31
sarowalhossain <sarowalhossain1997@gmail.com>
2021-06-03 18:48:31 +0600

    Added ViewBinding...

--- app/src/main/java/com/example/customtoolbar/ChildActivity.java
+++ app/src/main/java/com/example/customtoolbar/ChildActivity.java
@@ -7,21 +7,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.customtoolbar.databinding.ActivityChildBinding;
+import com.example.customtoolbar.databinding.ActivityMainBinding;
+
 import java.util.Objects;
 
 public class ChildActivity extends AppCompatActivity {
 
+    private ActivityChildBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_child);
+        //setContentView(R.layout.activity_child);
+        binding = ActivityChildBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Show back arrow in toolbar
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-    }
 
-    public void onButtonClicked(View view) {
-        onBackPressed();
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/customtoolbar/MainActivity.java
+++ app/src/main/java/com/example/customtoolbar/MainActivity.java
@@ -1,37 +1,35 @@
 package com.example.customtoolbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.customtoolbar.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        TextView textView = findViewById(R.id.appbar_textview_profile);
+        setSupportActionBar(binding.includedLayout.toolbar);
 
-        // this doesn't work
-        textView.setOnClickListener(new View.OnClickListener() {
+        binding.includedLayout.appbarTextviewProfile.setText("It's worked");
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, ChildActivity.class));
             }
         });
-
-    }
-
-    public void onButtonClicked(View view) {
-        startActivity(new Intent(this, ChildActivity.class));
     }
 }
